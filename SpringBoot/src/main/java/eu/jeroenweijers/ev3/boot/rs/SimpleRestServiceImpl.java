63d3881717a6df0487a38d7ca75b1b53f627b11f
Jeroen Weijers <jeroenweijers@gmail.com>
2017-03-03 16:04:43 +0100

    Parent gradle

--- SpringBoot/src/main/java/eu/jeroenweijers/ev3/boot/rs/SimpleRestServiceImpl.java
+++ SpringBoot/src/main/java/eu/jeroenweijers/ev3/boot/rs/SimpleRestServiceImpl.java
@@ -4,10 +4,6 @@ import lejos.hardware.Button;
 import lejos.hardware.lcd.LCD;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-
 @Component
 public class SimpleRestServiceImpl implements SimpleRestService{
 
